5b2fa411772848027864d1be7038b50e989df6ea
Alan Kalach <127144035+AlanKalach@users.noreply.github.com>
2024-08-27 00:42:44 -0600

    Update Fanatsy_auction_optimizer_app.py

--- Fanatsy_auction_optimizer_app.py
+++ Fanatsy_auction_optimizer_app.py
@@ -139,8 +139,7 @@ def run_optimizer(roster_data, scoring, players_df):
     
     # Update roster_history list
     roster_history += [roster.copy()] 
-    return roster
-""" 
+
     #Iteration 1-n -------------------------------------------------------------------------
     count = 1
     iterate = True 
@@ -217,7 +216,7 @@ def run_optimizer(roster_data, scoring, players_df):
         for col in ['Player', 'Avg. Salary (AVG)', 'Proj 23']:
             column_dict[col].extend(df[col])
     roster_evolution = pd.DataFrame(column_dict)
-"""
+    return roster
     
 
 # Button to run the program
